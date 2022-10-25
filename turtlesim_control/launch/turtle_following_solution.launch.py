6de78e1921e63d3f7ad3263446b152e3321a222b
Pi Thanacha Choopojcharoen <tchoopojcharoen@gmail.com>
2022-10-26 04:27:43 +0700

    Update turtle_following_solution.launch.py

--- turtlesim_control/launch/turtle_following_solution.launch.py
+++ turtlesim_control/launch/turtle_following_solution.launch.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler, LogInfo, EmitEvent, OpaqueFunction
+from launch.event_handlers import OnProcessStart, OnProcessExit
+from launch.events import Shutdown
+
 def generate_launch_description():
+
     gain = LaunchConfiguration('gain')
     gain_launch_arg = DeclareLaunchArgument('gain',default_value='5.0')
     
+
+    turtlesim = Node(
+        package='turtlesim',
+        executable='turtlesim_node'
+    )
     turtlesim = Node(
         package='turtlesim',
         executable='turtlesim_node'
@@ -15,6 +24,27 @@ def generate_launch_description():
         cmd = [['ros2 service call /spawn turtlesim/srv/Spawn "{x: 2.0, y: 2.0, theta: 0.0, name: \'turtle2\'}"']],
         shell=True
     )
+
+    spawn_event = RegisterEventHandler(
+        OnProcessStart(
+            target_action=turtlesim,
+            on_start=[
+                LogInfo(msg='spawning turtle...'),
+                spawn_turtle2
+            ]
+        )
+    )
+
+    sim_close_event = RegisterEventHandler(
+        OnProcessExit(
+            target_action=turtlesim,
+            on_exit=[
+                LogInfo(msg='Turtlesim is closed.'),
+                EmitEvent(event=Shutdown(reason='Turtlesim is closed.'))
+            ]
+        )
+    )
+
 
     leader = Node(
         package='turtlesim_control',
@@ -47,7 +77,8 @@ def generate_launch_description():
     launch_description = LaunchDescription()
     launch_description.add_action(gain_launch_arg)
     launch_description.add_action(turtlesim)
-    launch_description.add_action(spawn_turtle2)
+    launch_description.add_action(spawn_event)
+    launch_description.add_action(sim_close_event)
     launch_description.add_action(follower)
     launch_description.add_action(leader)
     launch_description.add_action(scheduler)
