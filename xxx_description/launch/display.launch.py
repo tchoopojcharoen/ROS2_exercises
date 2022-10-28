8149412e6f913c9bf9c8319f9dc306370585f10f
Pi Thanacha Choopojcharoen <tchoopojcharoen@gmail.com>
2022-10-28 08:04:40 +0700

    Update display.launch.py

--- xxx_description/launch/display.launch.py
+++ xxx_description/launch/display.launch.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
-import sys 
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import xacro
-#sudo apt install ros-foxy-joint-state-publisher
-#sudo apt install ros-foxy-joint-state-publisher-gui
+
 
 def launch_action_rviz(pkg_path,rviz_file_name):
     rviz_file_path = os.path.join(pkg_path, 'rviz', rviz_file_name)
@@ -26,12 +22,8 @@ def launch_action_rviz(pkg_path,rviz_file_name):
 
 def launch_action_robot_state_publisher(pkg_path,robot_file):
     file_path = os.path.join(pkg_path, 'robot',robot_file)
-    if robot_file.endswith('.urdf'):
-        with open(file_path, 'r') as infp:
-            robot_desc = infp.read()
-    elif robot_file.endswith('.xacro'):
-        robot_desc = xacro.process_file(file_path).toxml()
-        #robot_desc = xacro.process_file(robot_desc_path,mappings={'robot_name' : 'xxx1'}).toxml()
+    with open(file_path, 'r') as infp:
+        robot_desc = infp.read()
     
     robot_state_publisher = Node(package='robot_state_publisher',
                                   executable='robot_state_publisher',
