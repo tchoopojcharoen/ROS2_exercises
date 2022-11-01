a633f97610315abb45d847cc5c6739d00312c519
Pi Thanacha Choopojcharoen <tchoopojcharoen@gmail.com>
2022-11-02 00:02:32 +0700

    Update DH2Transform.py

--- manipulator/dummy_description/dummy_description/DH2Transform.py
+++ manipulator/dummy_description/dummy_description/DH2Transform.py
@@ -10,6 +10,8 @@ See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. 
 If not, see <https://www.gnu.org/licenses/>.
+
+created by Thanacha Choopojcharoen at CoXsys Robotics (2022)
 """
 
 # all neccesary libraries
