a096ef23985b114b5bf2c3b99f782ed84d7bc24f
Pi Thanacha Choopojcharoen <tchoopojcharoen@gmail.com>
2022-11-02 00:01:07 +0700

    Update trajectory.py

--- manipulator/dummy_control/dummy_control/trajectory.py
+++ manipulator/dummy_control/dummy_control/trajectory.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 This program is free software: you can redistribute it and/or modify it 
 under the terms of the GNU General Public License as published by the Free Software Foundation, 
@@ -10,7 +11,10 @@ See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. 
 If not, see <https://www.gnu.org/licenses/>.
+
+created by Thanacha Choopojcharoen at CoXsys Robotics (2022)
 """
+
 import numpy as np
 from scipy.optimize import minimize
 
