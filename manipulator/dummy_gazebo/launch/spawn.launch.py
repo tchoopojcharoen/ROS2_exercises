e23f56aebc99184cd0bf3b36ccedb06bffbe34f5
Pi Thanacha Choopojcharoen <tchoopojcharoen@gmail.com>
2022-11-09 09:54:49 +0700

    Add files via upload

---
+++ manipulator/dummy_gazebo/launch/spawn.launch.py
@@ -0,0 +1,165 @@
+#!usr/bin/python3
+
+"""
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see <https://www.gnu.org/licenses/>.
+
+created by Thanacha Choopojcharoen at CoXsys Robotics (2022)
+"""
+
+import os, yaml
+import xacro
+from launch import LaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, LogInfo, TimerAction
+from launch.event_handlers import OnProcessStart, OnProcessExit, OnExecutionComplete
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+from dummy_description.DH2Transform import DH2Transform
+class ShareFile():
+    def __init__(self,package,folder,file):
+        self.package_name = package
+        self.package_path = get_package_share_directory(package)
+        self.folder = folder
+        self.file = file
+        self.path = os.path.join(self.package_path,folder,file)
+def launch_action_gazebo():
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzserver.launch.py'
+            ])
+        ])
+    )
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzclient.launch.py'
+            ])
+        ])
+    )
+    return gazebo_server,gazebo_client
+def launch_action_robot_spawner(dh_parameters,robot_description,controller,position,robot_name=''):
+    # robot_state_publisher
+    DH2Transform(dh_parameters.package_name,dh_parameters.folder,dh_parameters.file) 
+    parameters = []
+    if robot_name:
+        robot_desc_xml = xacro.process_file(robot_description.path,mappings={'robot_name':robot_name}).toxml()
+        parameters.append({'frame_prefix':robot_name+'/'})
+    else:
+        robot_desc_xml = xacro.process_file(robot_description.path).toxml()
+    parameters.append({'robot_description':robot_desc_xml})
+    parameters.append({'use_sim_time': True})
+    
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=parameters,
+        namespace=robot_name
+    )    
+
+    controller_config = generate_controller_config(controller,robot_name)
+
+    spawner = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        output='screen',
+        arguments=[
+            '-topic', robot_name+'/robot_description',
+            '-entity', robot_name+'/dummy',
+            '-x', str(position[0]),
+            '-y', str(position[1]),
+            '-z', str(position[2]),
+            '-R', '0.0',
+            '-P', '0.0',
+            '-Y', '0.0',
+        ]
+    )
+
+    joint_trajectory_controller = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        arguments=['joint_trajectory_position_controller','--controller-manager',robot_name+'/controller_manager']
+    )
+
+    joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        arguments=['joint_state_broadcaster','--controller-manager',robot_name+'/controller_manager']
+    )
+
+    trajectory_generator = Node(
+        package='dummy_control',
+        executable='trajectory_generator.py',
+        namespace=robot_name,
+        parameters=[controller_config]
+    )
+    
+    actions = []
+    actions.append(robot_state_publisher)
+    actions.append(spawner)
+    actions.append(joint_trajectory_controller)
+    actions.append(trajectory_generator)
+    actions.append(joint_state_broadcaster)
+
+    return actions
+def generate_controller_config(controller:ShareFile,namespace):
+    with open(controller.path,'r') as file:
+        doc = yaml.load(file,yaml.SafeLoader)
+    ###
+    new_doc = {namespace:doc}
+    ###
+    new_controller_path = os.path.join(controller.package_path,controller.folder,namespace+controller.file)
+    with open(new_controller_path,'w') as file:
+        yaml.dump(new_doc,file)
+    return new_controller_path
+def generate_launch_description():
+    dh_parameters = ShareFile('dummy_description','config','DH_parameters.yaml')
+    robot_description = ShareFile('dummy_gazebo','robot','dummy.xacro')
+    controller = ShareFile('dummy_gazebo','config','_controller_config.yaml')
+    
+    gazebo_server,gazebo_client = launch_action_gazebo()
+    launch_description = LaunchDescription()
+    launch_description.add_action(gazebo_server)
+    launch_description.add_action(gazebo_client)
+    
+    N = 3
+    for i in range(N):
+        robot_name = 'dummy_'+str(i+1)
+        actions = launch_action_robot_spawner(
+            dh_parameters,
+            robot_description,
+            controller,
+            [0.0,-2*i,0.0],
+            robot_name
+        )
+        if i == 0 :
+            for action in actions:
+                launch_description.add_action(action)  
+        else:
+            spawn_event = RegisterEventHandler(
+                OnProcessExit(
+                    target_action=previous_action,
+                    on_exit= TimerAction(period=2.0,actions=actions)
+                )
+            )
+            launch_description.add_action(spawn_event)
+        previous_action = actions[-1]
+
+    return launch_description
+    
---
+++ manipulator/dummy_kinematics/scripts/kinematics_server.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+
+"""
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see <https://www.gnu.org/licenses/>.
+
+created by Thanacha Choopojcharoen at CoXsys Robotics (2022)
+"""
+
+import sys, os, yaml
+import rclpy
+from rclpy.node import Node
+from rclpy.duration import Duration
+from sensor_msgs.msg import JointState
+from dummy_kinematics_interfaces.srv import SolveIK, SetConfig
+from dummy_kinematics.kinematics import inverse_kinematics
+from ament_index_python.packages import get_package_share_directory
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+class KinematicsServer(Node):
+    def __init__(self):
+        super().__init__('kinematics_server')
+        self.joint_state_publisher = self.create_publisher(JointState,'/joint_states',10)
+        self.IK_server = self.create_service(SolveIK,'solve_ik',self.IK_callback)
+        self.FK_server = self.create_service(SetConfig,'set_config',self.set_config_callback)
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer,self)
+
+        self.get_logger().info(f'Starting kinematics solver with node name {self.get_node_names()[0]}')
+        description_pkg_path = get_package_share_directory('dummy_description')
+        parameter_path = os.path.join(description_pkg_path,'config','DH_parameters.yaml')
+        with open(parameter_path) as f:
+            self.param = yaml.load(f,Loader=yaml.SafeLoader)
+        self.joint_names = ['joint_1','joint_2','joint_3']
+        msg = JointState()
+        msg.name = self.joint_names
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.position = [0.,0.,0.]
+        self.joint_states = msg
+        self.timer = self.create_timer(0.1,self.timer_callback)
+    def timer_callback(self):
+        self.joint_states.header.stamp = self.get_clock().now().to_msg()
+        self.joint_state_publisher.publish(self.joint_states)
+        
+    def set_config_callback(self,requst:SetConfig.Request,response:SetConfig.Response):
+        self.joint_states.header.stamp = self.get_clock().now().to_msg()
+        self.joint_states.position = requst.joint_states.position
+        self.joint_state_publisher.publish(self.joint_states)
+        trans = self.tf_buffer.lookup_transform('base_link','end_effector',Duration(seconds=0))
+        
+        response.position.x = trans.transform.translation.x
+        response.position.y = trans.transform.translation.y
+        response.position.z = trans.transform.translation.z
+        
+        return response
+    def IK_callback(self,request:SolveIK.Request,response:SolveIK.Response):
+        p = (request.position.x,request.position.y,request.position.z)
+        gamma_list = [1,1]
+        idx = 0
+        for g in request.arm_config.data :
+            if (g != 1) & (g != -1):
+                self.get_logger().warning(f'The arm configuration [{idx}] should be an array of 1 or -1.')
+                if g>=0:
+                    gamma_list[idx] = 1
+                    self.get_logger().warning(f'The positive arm configuration [{idx}] is mapped to gamma = 1')
+                else:
+                    gamma_list[idx] = -1
+                    self.get_logger().warning(f'The negative arm configuration [{idx}] is mapped to gamma = -1')
+            else:
+                gamma_list[idx] = g
+            idx = idx+1
+        gamma = (gamma_list[0],gamma_list[1])
+        l_1 = self.param['DH'][1][1]
+        l_2 = self.param['DH'][2][1]
+        l_3 = self.param['ee_position'][0]
+        h_1 = self.param['DH'][0][3]
+        h_3 = self.param['ee_position'][1]
+        q,flag = inverse_kinematics(p,gamma,l_1,l_2,l_3,h_1,h_3)
+        
+        for q_i in q:
+            response.joint_states.position.append(q_i)
+        response.flag.data = flag
+        if flag:
+            response.joint_states.name = self.joint_names
+            response.joint_states.header.stamp = self.get_clock().now().to_msg()
+            self.joint_states = response.joint_states
+        return response
+
+def main(args=None):
+
+    rclpy.init(args=args)
+    node = KinematicsServer()
+    try:
+        while rclpy.ok():
+            rclpy.spin_once(node)
+    except KeyboardInterrupt:
+        print('repeater stopped cleanly')
+    except BaseException:
+        print('exception in repeater:', file=sys.stderr)
+        raise
+    finally:
+        node.destroy_node()
+        rclpy.shutdown() 
+
+if __name__=='__main__':
+    main()
